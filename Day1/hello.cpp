639c7f7b6c24f88a492b55b87d238fe8d7462187
TRP2007 <tanujapokharkar63@gmail.com>
2026-07-13 17:29:46 +0000

    Day 1: Added Hello World program

---
+++ Day1/hello.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"omg hello google this is my 1st step towards you...i am so excited to LEARN,EXPLORE AND EARN"<<endl;
+    return 0;
+}
